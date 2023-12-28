c7cbbb1c0ae2060b14a42f6ef457cb00b817e621
hb9397 <hb9397@naver.com>
2023-12-28 16:08:11 +0900

    🎉init: Auth 애플리케이션 생성

---
+++ src/main/java/com/sesac/auth_server_v1/AuthServerV1Application.java
@@ -0,0 +1,13 @@
+package com.sesac.auth_server_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesac/auth_server_v1/AuthServerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.auth_server_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
