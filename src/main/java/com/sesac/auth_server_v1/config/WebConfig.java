589f0d57f8b62b99774b3beba4138bcee5809865
hb9397 <hb9397@naver.com>
2024-01-06 13:35:18 +0900

    🐛 fix: CORS 설정 수정
    - SecurityFilter 를 사용하지 않는 형태로 수정
    
    - version 0.0.0

--- src/main/java/com/sesac/auth_server_v1/config/WebConfig.java
+++ src/main/java/com/sesac/auth_server_v1/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.sesac.auth_server_v1.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,24 +25,25 @@ public class WebConfig implements WebMvcConfigurer {
 	private final JwtInterceptor jwtInterceptor;
 
 	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 
 		// 개발 환경
-		config.addAllowedOrigin("*");
-
 		config.addAllowedOrigin(clientAddr);
 		config.addAllowedMethod("*");
 		config.addAllowedHeader("*");
 		config.setAllowCredentials(true);
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
 		source.registerCorsConfiguration("/**", config);
-		return source;
+		FilterRegistrationBean<CorsFilter> corsBean = new FilterRegistrationBean<>(new CorsFilter(source));
+		corsBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return corsBean;
 	}
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(jwtInterceptor)
-			//.addPathPatterns("/**")
-			.excludePathPatterns("/**");
+			.addPathPatterns("/**")
+			.excludePathPatterns("/api/v1/authentication/member");
 	}
 }
